d9b8bcd8d58113c6bef398e9b7142f914c337130
Brian Graham <anthony.graham12@pm.me>
2024-11-21 00:05:46 -0600

    Add UFW Configs
    Add MySQL and MariaDB UFW config
    Add HTTP and HTTPS UFW config

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
+# Written by Brian Graham.
+
 # Run Critical Updates
 
 # Debian Based OS
+
+#!/bin/sh
+
 if [ -f /etc/debian_version ]; then
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+    echo "Debian-based system detected. Running updates..."
+    sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 else
-echo "Probably not Debian/Ubuntu, moving on!"
+    echo "This is probably not a Debian/Ubuntu-based system. Moving on!"
 fi
+
 
 # CentOS/RHEL/Alma/Rocky Based OS
 
@@ -16,10 +23,30 @@ if [ -f /etc/os-release ]; then
             echo "This is a CentOS/RHEL/AlmaLinux-based system."
             sudo yum upgrade -y && sudo yum autoremove -y
             ;;
-        *)
+        else
             echo "This is not a CentOS/RHEL/AlmaLinux-based system."
             ;;
     esac
 else
     echo "Cannot determine OS! Aborting..."
 fi
+
+# Configure UFW if it's installed.
+
+#!/bin/sh
+
+if [ -f /etc/debian_version ]; then
+    if command -v ufw > /dev/null 2>&1; then
+        echo "UFW is installed. Configuring firewall..."
+        sudo ufw allow 443/tcp && sudo ufw allow 80/tcp
+        if command -v mysql > /dev/null 2>&1 || command -v mariadb > /dev/null 2>&1; then
+            echo "MySQL or MariaDB detected. Allowing port 3306..."
+            sudo ufw allow 3306/tcp
+        fi
+    else
+        echo "UFW is not installed. Skipping firewall configuration."
+    fi
+else
+    echo "This is not a Debian-based system. Exiting..."
+fi
+
